e56785b4c3cf72a3b8f5199a6cfdc7bad33628df
Charlie Gooderham <cgooderham94@gmail.com>
2022-12-14 12:14:57 +0000

    Adds plaecholder feedback data. Displays list of data.

--- src/containers/FeedbackFlow/FeedbackFlow.tsx
+++ src/containers/FeedbackFlow/FeedbackFlow.tsx
@@ -1,5 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+
+interface Feedback {
+  name: string;
+  email: string;
+  rating: number;
+  comment: string;
+}
+
+type FeedbackList = Feedback[];
 
 export const FeedbackFlow = () => {
-  return <div>Feedback flow component</div>;
+  const [feedbackList, setFeedbackList] = useState<FeedbackList>([
+    {
+      name: "Joe Bloggs",
+      email: "joe@bloggs.com",
+      rating: 5,
+      comment: "Fantastic product! Thanks.",
+    },
+    {
+      name: "Jack Sparrow",
+      email: "jack@sparrow.com",
+      rating: 4,
+      comment: "Nicely refined product. Great customer service.",
+    },
+  ]);
+
+  return (
+    <ul>
+      {feedbackList.map(({ name, email, rating, comment }) => (
+        <li>
+          <div>Name: {name}</div>
+          <div>Email: {email}</div>
+          <div>Rating: {rating}</div>
+          <div>Comment: {comment}</div>
+        </li>
+      ))}
+    </ul>
+  );
 };
