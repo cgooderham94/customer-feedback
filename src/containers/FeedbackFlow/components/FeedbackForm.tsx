a345f48a13f1742a7d062f6b04eef4937dedeeea
Charlie Gooderham <cgooderham94@gmail.com>
2022-12-14 12:39:34 +0000

    Implementis basic form fields and button.

--- src/containers/FeedbackFlow/components/FeedbackForm.tsx
+++ src/containers/FeedbackFlow/components/FeedbackForm.tsx
@@ -1,3 +1,16 @@
+import { Button, TextField } from "@mui/material";
+import { FEEDBACK_FORM_CONTENT } from "./constants";
+
+const { name, email, rating, comment } = FEEDBACK_FORM_CONTENT;
+
 export const FeedbackForm = () => (
-  <form>Customer feedback form fields here</form>
+  <form>
+    <TextField id={name.id} label={name.label} />
+    <TextField id={email.id} label={email.label} type="email" />
+    <TextField id={rating.id} label={rating.label} type="number" />
+    <TextField id={comment.id} label={comment.label} multiline />
+    <Button type="submit" variant="contained">
+      Submit
+    </Button>
+  </form>
 );
---
+++ src/containers/FeedbackFlow/components/constants.ts
@@ -0,0 +1,22 @@
+export const FEEDBACK_FORM_CONTENT = {
+  name: {
+    label: "Name",
+    id: "name",
+    type: "text",
+  },
+  email: {
+    label: "Email",
+    id: "email",
+    type: "email",
+  },
+  rating: {
+    label: "Rating (1-5 stars)",
+    id: "rating",
+    type: "number",
+  },
+  comment: {
+    label: "Comment",
+    id: "comment",
+    type: "textarea",
+  },
+};
