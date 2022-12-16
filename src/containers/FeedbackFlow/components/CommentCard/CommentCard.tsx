b504f28f1edebed1249528b19b6bcb44b920ae99
Charlie Gooderham <cgooderham94@gmail.com>
2022-12-16 16:30:35 +0000

    Remove redundant prop from comment card

--- src/containers/FeedbackFlow/components/CommentCard/CommentCard.tsx
+++ src/containers/FeedbackFlow/components/CommentCard/CommentCard.tsx
@@ -1,21 +1,19 @@
-import { FC, ReactElement } from "react";
-import { Grid, Rating } from "@mui/material";
+import { FC } from "react";
+import { Rating } from "@mui/material";
 import { Card } from "./CommentCard.styles";
 
 interface CommentCardProps {
-  key: ReactElement["key"];
   email: string;
   rating: number;
   comment: string;
 }
 
 export const CommentCard: FC<CommentCardProps> = ({
-  key,
   email,
   rating,
   comment,
 }) => (
-  <Card key={key} variant="outlined">
+  <Card variant="outlined">
     <div>{email}</div>
     <Rating value={rating} readOnly />
     <div>{comment}</div>
